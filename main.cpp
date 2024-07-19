e8e3fa6ff68ce7584a74da3c0a2b92c2085c2828
JoseVasquez1104 <profefarias80@gmail.com>
2024-07-18 20:23:10 -0400

    poniendo el int main y probando el programa

--- main.cpp
+++ main.cpp
@@ -34,3 +34,58 @@ double promedio(int cantidad, double* valores) {
     }
     return suma / cantidad;
 }
+
+int main() {
+    int opcion;
+    do {
+        cout << "Menú de opciones:" << endl;
+        cout << "1. Calcular factorial" << endl;
+        cout << "2. Calcular valor absoluto" << endl;
+        cout << "3. Calcular promedio genérico" << endl;
+        cout << "4. Salir" << endl;
+        cout << "Ingrese una opción: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1: {
+                int n;
+                cout << "Ingrese el valor de n: ";
+                cin >> n;
+                long long resultado = factorial(n);
+                if (resultado != -1) {
+                    cout << "El factorial de " << n << " es: " << resultado << endl;
+                }
+                break;
+            }
+            case 2: {
+                double x;
+                cout << "Ingrese el valor de x: ";
+                cin >> x;
+                double resultado = absoluto(x);
+                cout << "El valor absoluto de " << x << " es: " << resultado << endl;
+                break;
+            }
+            case 3: {
+                int cantidad;
+                cout << "Ingrese la cantidad de valores: ";
+                cin >> cantidad;
+                double* valores = new double[cantidad];
+                for (int i = 0; i < cantidad; i++) {
+                    cout << "Ingrese el valor " << i + 1 << ": ";
+                    cin >> valores[i];
+                }
+                double resultado = promedio(cantidad, valores);
+                cout << "El promedio es: " << resultado << endl;
+                delete[] valores;
+                break;
+            }
+            case 4:
+                cout << "Saliendo del programa..." << endl;
+                break;
+            default:
+                cout << "Opción inválida. Intente nuevamente." << endl;
+        }
+    } while (opcion != 4);
+
+    return 0;
+}
